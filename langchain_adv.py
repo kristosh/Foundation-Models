082b4030d6208acecbb722c2bd451cf332b877c0
kristosh <chrathans@gmail.com>
2025-02-13 17:01:48 +0100

    updates

--- langchain_adv.py
+++ langchain_adv.py
@@ -1,7 +1,10 @@
 import os
 os.environ['HF_HOME'] = "/local/athanasiadisc/cache"
 # pip install llama-cpp-python
-from langchain import LlamaCpp
+from langchain import LlamaCpp, PromptTemplate, LLMChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.memory import ConversationSummaryMemory
+import pdb
 
 # Make sure the model path is correct for your system!
 llm = LlamaCpp(
@@ -14,10 +17,8 @@ llm = LlamaCpp(
 )
 
 llm.invoke("Hi! My name is Maarten. What is 1 + 1?")
-
-from langchain import PromptTemplate
-
 # Create a prompt template with the "input_prompt" variable
+
 template = """<s><|user|>
 {input_prompt}<|end|>
 <|assistant|>"""
@@ -27,7 +28,6 @@ prompt = PromptTemplate(
 )
 
 basic_chain = prompt | llm
-
 
 # Use the chain
 basic_chain.invoke(
@@ -43,16 +43,12 @@ name_prompt = PromptTemplate(
     input_variables=["product"]
 )
 
-
-from langchain import LLMChain
-
 # Create a chain for the title of our story
 template = """<s><|user|>
 Create a title for a story about {summary}. Only return the title.<|end|>
 <|assistant|>"""
 title_prompt = PromptTemplate(template=template, input_variables=["summary"])
 title = LLMChain(llm=llm, prompt=title_prompt, output_key="title")
-
 title.invoke({"summary": "a girl that lost her mother"})
 
 # Create a chain for the character description using the summary and title
@@ -63,9 +59,6 @@ character_prompt = PromptTemplate(
     template=template, input_variables=["summary", "title"]
 )
 character = LLMChain(llm=llm, prompt=character_prompt, output_key="character")
-
-import pdb
-pdb.set_trace()
 
 # Create a chain for the story using the summary, title, and character description
 template = """<s><|user|>
@@ -78,8 +71,93 @@ story = LLMChain(llm=llm, prompt=story_prompt, output_key="story")
 
 # Combine all three components to create the full chain
 llm_chain = title | character | story
-
 llm_chain.invoke("a girl that lost her mother")
-
 # Let's give the LLM our name
 basic_chain.invoke({"input_prompt": "Hi! My name is Maarten. What is 1 + 1?"})
+# Create an updated prompt template to include a chat history
+template = """<s><|user|>Current conversation:{chat_history}
+
+{input_prompt}<|end|>
+<|assistant|>"""
+
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["input_prompt", "chat_history"]
+)
+
+# Define the type of memory we will use
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+# Chain the LLM, prompt, and memory together
+llm_chain = LLMChain(
+    prompt=prompt,
+    llm=llm,
+    memory=memory
+)
+
+# Generate a conversation and ask a basic question
+llm_chain.invoke({"input_prompt": "Hi! My name is Maarten. What is 1 + 1?"})
+
+# Does the LLM remember the name we gave it?
+llm_chain.invoke({"input_prompt": "What is my name?"})
+
+# Retain only the last 2 conversations in memory
+memory = ConversationBufferWindowMemory(k=2, memory_key="chat_history")
+
+# Chain the LLM, prompt, and memory together
+llm_chain = LLMChain(
+    prompt=prompt,
+    llm=llm,
+    memory=memory
+)
+
+# Ask two questions and generate two conversations in its memory
+llm_chain.predict(input_prompt="Hi! My name is Maarten and I am 33 years old. What is 1 + 1?")
+llm_chain.predict(input_prompt="What is 3 + 3?")
+
+# Check whether it knows the name we gave it
+llm_chain.invoke({"input_prompt":"What is my name?"})
+
+# Check whether it knows the age we gave it
+llm_chain.invoke({"input_prompt":"What is my age?"})
+
+# Create a summary prompt template
+summary_prompt_template = """<s><|user|>Summarize the conversations and update with the new lines.
+
+Current summary:
+{summary}
+
+new lines of conversation:
+{new_lines}
+
+New summary:<|end|>
+<|assistant|>"""
+summary_prompt = PromptTemplate(
+    input_variables=["new_lines", "summary"],
+    template=summary_prompt_template
+)
+
+# Define the type of memory we will use
+memory = ConversationSummaryMemory(
+    llm=llm, 
+    memory_key="chat_history", 
+    prompt=summary_prompt
+)
+# Chain the LLM, prompt, and memory together
+llm_chain = LLMChain(
+    prompt=prompt,
+    llm=llm,
+    memory=memory
+)
+
+# Generate a conversation and ask for the name
+llm_chain.invoke({"input_prompt": "Hi! My name is Maarten. What is 1 + 1?"})
+llm_chain.invoke({"input_prompt": "What is my name?"})
+
+# Check whether it has summarized everything thus far
+llm_chain.invoke({"input_prompt": "What was the first question I asked?"})
+
+# Check what the summary is thus far
+memory.load_memory_variables({})
+
+pdb.set_trace()
