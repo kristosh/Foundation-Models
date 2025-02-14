035bf3b8795b62c44df39ae1f2247e64c5ae0a0c
kristosh <chrathans@gmail.com>
2025-02-14 16:20:21 +0100

    repo_updates

---
+++ clip.py
@@ -0,0 +1,3 @@
+import os
+os.environ['HF_HOME'] = "/local/athanasiadisc/cache"
+from timeit import default_timer as timer
--- rag.py
+++ rag.py
@@ -1,39 +1,33 @@
 
 import os
 os.environ['HF_HOME'] = "/local/athanasiadisc/cache"
+from timeit import default_timer as timer
 
 import cohere
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+def time_tick():
+    tick = timer()
+    return tick
+
+def duration2tcks(tick1, tick2):
+    return (tick2 - tick1)
+
+f = open('../../_files/rag_text.txt')
+texts = f.readlines()
 # Paste your API key here. Remember to not share publicly
 api_key = 'M2GKPBFIZeVVrDI447YcZ5fiWVlJ3dtTilCb9861'
 
 # Create and retrieve a Cohere API key from os.cohere.ai
 co = cohere.Client(api_key)
 
+# with open('../../_files/rag_text.txt') as text:
+#   s = " ".join([l.rstrip("\n") for l in text]) 
 
-text = """
-Interstellar is a 2014 epic science fiction film co-written, directed, and produced by Christopher Nolan. 
-It stars Matthew McConaughey, Anne Hathaway, Jessica Chastain, Bill Irwin, Ellen Burstyn, Matt Damon, and Michael Caine. 
-Set in a dystopian future where humanity is struggling to survive, the film follows a group of astronauts who travel through a wormhole near Saturn in search of a new home for mankind.
-
-Brothers Christopher and Jonathan Nolan wrote the screenplay, which had its origins in a script Jonathan developed in 2007. 
-Caltech theoretical physicist and 2017 Nobel laureate in Physics[4] Kip Thorne was an executive producer, acted as a scientific consultant, and wrote a tie-in book, The Science of Interstellar. 
-Cinematographer Hoyte van Hoytema shot it on 35 mm movie film in the Panavision anamorphic format and IMAX 70 mm. 
-Principal photography began in late 2013 and took place in Alberta, Iceland, and Los Angeles. 
-Interstellar uses extensive practical and miniature effects and the company Double Negative created additional digital effects.
-
-Interstellar premiered on October 26, 2014, in Los Angeles. 
-In the United States, it was first released on film stock, expanding to venues using digital projectors. 
-The film had a worldwide gross over $677 million (and $773 million with subsequent re-releases), making it the tenth-highest grossing film of 2014. 
-It received acclaim for its performances, direction, screenplay, musical score, visual effects, ambition, themes, and emotional weight. 
-It has also received praise from many astronomers for its scientific accuracy and portrayal of theoretical astrophysics. Since its premiere, Interstellar gained a cult following,[5] and now is regarded by many sci-fi experts as one of the best science-fiction films of all time.
-Interstellar was nominated for five awards at the 87th Academy Awards, winning Best Visual Effects, and received numerous other accolades"""
-
-# Split into a list of sentences
-texts = text.split('.')
+# # Split into a list of sentences
+# texts = text.split('.')
 
 # Clean up to remove empty spaces and new lines
 texts = [t.strip(' \n') for t in texts]
@@ -75,3 +69,62 @@ def search(query, number_of_results=3):
 query = "What is the mass of the moon?"
 results = search(query)
 print (results)
+
+from langchain import LlamaCpp
+
+# Make sure the model path is correct for your system!
+llm = LlamaCpp(
+    model_path="../../_files/Phi-3-mini-4k-instruct-fp16.gguf",
+    n_gpu_layers=-1,
+    max_tokens=500,
+    n_ctx=2048,
+    seed=42,
+    verbose=False
+)
+
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+
+# Embedding model for converting text to numerical representations
+embedding_model = HuggingFaceEmbeddings(
+    model_name='thenlper/gte-small'
+)
+
+
+from langchain.vectorstores import FAISS
+
+# Create a local vector database
+db = FAISS.from_texts(texts, embedding_model)
+
+from langchain import PromptTemplate
+
+# Create a prompt template
+template = """<|user|>
+Relevant information:
+{context}
+
+Provide a concise answer the following question using the relevant information provided above:
+{question}<|end|>
+<|assistant|>"""
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["context", "question"]
+)
+
+
+from langchain.chains import RetrievalQA
+
+# RAG pipeline
+rag = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type='stuff',
+    retriever=db.as_retriever(),
+    chain_type_kwargs={
+        "prompt": prompt
+    },
+    verbose=True
+)
+
+start = timer()
+temp = rag.invoke('Who did the  storyboarding and screenwriting?')
+end = timer()
+print(end - start)
