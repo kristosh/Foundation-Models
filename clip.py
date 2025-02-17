851e627c4a86b2704ad184007fcaa8caf89e388c
kristosh <chrathans@gmail.com>
2025-02-17 17:37:35 +0100

    updates

--- clip.py
+++ clip.py
@@ -1,5 +1,6 @@
 import os
 os.environ['HF_HOME'] = "/local/athanasiadisc/cache"
+
 from timeit import default_timer as timer
 from sentence_transformers import SentenceTransformer, util
 from urllib.request import urlopen
@@ -11,143 +12,145 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+import torch
+
 # Clip is a multimodal LLMs that leverages the embeddings from different modalities like text and images and Transformer architecure to coccurentrly project them into a 
 # common subspace
 # In this example we will see how clips works for simple images
 # Load an AI-generated image of a puppy playing in the snow
-puppy_path = "../../_files/solomon.jpg"
-image = Image.open((puppy_path)).convert("RGB")
-
+img_path = "../../_files/solomon.jpg"
 caption = "satelites in a garden"
-model_id = "openai/clip-vit-base-patch32"
 
-# Load a tokenizer to preprocess the text
-clip_tokenizer = CLIPTokenizerFast.from_pretrained(model_id)
+image = Image.open(img_path).convert("RGB")
 
-# Load a processor to preprocess the images
-clip_processor = CLIPProcessor.from_pretrained(model_id)
+def clip_model(image):
 
-# Main model for generating text and image embeddings
-model = CLIPModel.from_pretrained(model_id)
+    model_id = "openai/clip-vit-base-patch32"
 
-# Tokenize our input
-inputs = clip_tokenizer(caption, return_tensors="pt")
-inputs 
+    # Load a tokenizer to preprocess the text
+    clip_tokenizer = CLIPTokenizerFast.from_pretrained(model_id)
 
-# Convert our input back to tokens
-clip_tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
+    # Load a processor to preprocess the images
+    clip_processor = CLIPProcessor.from_pretrained(model_id)
 
-# Create a text embedding
-text_embedding = model.get_text_features(**inputs)
-text_embedding.shape
+    # Main model for generating text and image embeddings
+    model = CLIPModel.from_pretrained(model_id)
 
-# Preprocess image
-processed_image = clip_processor(
-    text=None, images=image, return_tensors="pt"
-)["pixel_values"]
+    # Tokenize our input
+    inputs = clip_tokenizer(caption, return_tensors="pt")
+    inputs 
 
-# Prepare image for visualization
-img = processed_image.squeeze(0)
-img = img.permute(*torch.arange(img.ndim - 1, -1, -1))
-img = np.einsum("ijk->jik", img)
+    # Convert our input back to tokens
+    clip_tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
 
-# # Visualize preprocessed image
-# plt.imshow(img)
-# plt.axis("off")
-# plt.savefig('../../_files/image.png')
+    # Create a text embedding
+    text_embedding = model.get_text_features(**inputs)
+    text_embedding.shape
+
+    # Preprocess image
+    processed_image = clip_processor(
+        text=None, images=image, return_tensors="pt"
+    )["pixel_values"]
+
+    # Prepare image for visualization
+    img = processed_image.squeeze(0)
+    img = img.permute(*torch.arange(img.ndim - 1, -1, -1))
+    img = np.einsum("ijk->jik", img)
+
+    # # Visualize preprocessed image
+    # plt.imshow(img)
+    # plt.axis("off")
+    # plt.savefig('../../_files/image.png')
 
 
-# Create the image embedding
-image_embedding = model.get_image_features(processed_image)
-image_embedding.shape
+    # Create the image embedding
+    image_embedding = model.get_image_features(processed_image)
+    image_embedding.shape
 
-# Normalize the embeddings
-text_embedding /= text_embedding.norm(dim=-1, keepdim=True)
-image_embedding /= image_embedding.norm(dim=-1, keepdim=True)
+    # Normalize the embeddings
+    text_embedding /= text_embedding.norm(dim=-1, keepdim=True)
+    image_embedding /= image_embedding.norm(dim=-1, keepdim=True)
 
-# Calculate their similarity
-text_embedding = text_embedding.detach().cpu().numpy()
-image_embedding = image_embedding.detach().cpu().numpy()
-score = np.dot(text_embedding, image_embedding.T)
+    # Calculate their similarity
+    text_embedding = text_embedding.detach().cpu().numpy()
+    image_embedding = image_embedding.detach().cpu().numpy()
+    score = np.dot(text_embedding, image_embedding.T)
 
-# Load SBERT-compatible CLIP model
-model = SentenceTransformer("clip-ViT-B-32")
+    # Load SBERT-compatible CLIP model
+    model = SentenceTransformer("clip-ViT-B-32")
 
-images = image
-captions = caption
-# Encode the images
-image_embeddings = model.encode(images)
+    images = image
+    captions = caption
+    # Encode the images
+    image_embeddings = model.encode(images)
 
-# Encode the captions
-text_embeddings = model.encode(captions)
+    # Encode the captions
+    text_embeddings = model.encode(captions)
 
-#Compute cosine similarities
-sim_matrix = util.cos_sim(
-    image_embeddings, text_embeddings
-)
+    #Compute cosine similarities
+    sim_matrix = util.cos_sim(
+        image_embeddings, text_embeddings
+    )
+
+    return sim_matrix
 
 
 # code on blip-2
 
-from transformers import AutoProcessor, Blip2ForConditionalGeneration
-import torch
+def blip_model(image):
+    # Load processor and main model
+    blip_processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+    model = Blip2ForConditionalGeneration.from_pretrained(
+        "Salesforce/blip2-opt-2.7b",
+        torch_dtype=torch.float16
+    )
 
-# Load processor and main model
-blip_processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
-model = Blip2ForConditionalGeneration.from_pretrained(
-    "Salesforce/blip2-opt-2.7b",
-    torch_dtype=torch.float16
-)
+    # Send the model to GPU to speed up inference
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
 
-# Send the model to GPU to speed up inference
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
+    # Preprocess the image
+    inputs = blip_processor(image, return_tensors="pt").to(device, torch.float16)
+    inputs["pixel_values"].shape
 
-# Load image of a supercar
-car_path = "https://raw.githubusercontent.com/HandsOnLLM/Hands-On-Large-Language-Models/main/chapter09/images/car.png"
-image = Image.open(urlopen(car_path)).convert("RGB")
+    # Preprocess the text
+    text = "Her vocalization was remarkably melodic"
+    token_ids = blip_processor(image, text=text, return_tensors="pt")
+    token_ids = token_ids.to(device, torch.float16)["input_ids"][0]
 
-# Preprocess the image
-inputs = blip_processor(image, return_tensors="pt").to(device, torch.float16)
-inputs["pixel_values"].shape
+    # Convert input ids back to tokens
+    tokens = blip_processor.tokenizer.convert_ids_to_tokens(token_ids)
 
-# Preprocess the text
-text = "Her vocalization was remarkably melodic"
-token_ids = blip_processor(image, text=text, return_tensors="pt")
-token_ids = token_ids.to(device, torch.float16)["input_ids"][0]
+    # Replace the space token with an underscore
+    tokens = [token.replace("Ġ", "_") for token in tokens]
 
-# Convert input ids back to tokens
-tokens = blip_processor.tokenizer.convert_ids_to_tokens(token_ids)
-tokens
-
-# Replace the space token with an underscore
-tokens = [token.replace("Ġ", "_") for token in tokens]
-
-# Load an AI-generated image of a supercar
-image = Image.open(urlopen(car_path)).convert("RGB")
+    # Load an AI-generated image of a supercar
+    image = Image.open(urlopen(car_path)).convert("RGB")
 
 
-# Convert an image into inputs and preprocess it
-inputs = blip_processor(image, return_tensors="pt").to(device, torch.float16)
+    # Convert an image into inputs and preprocess it
+    inputs = blip_processor(image, return_tensors="pt").to(device, torch.float16)
 
-# Generate image ids to be passed to the decoder (LLM)
-generated_ids = model.generate(**inputs, max_new_tokens=20)
+    # Generate image ids to be passed to the decoder (LLM)
+    generated_ids = model.generate(**inputs, max_new_tokens=20)
 
-# Generate text from the image ids
-generated_text = blip_processor.batch_decode(
-    generated_ids, skip_special_tokens=True
-)
-generated_text = generated_text[0].strip()
+    # Generate text from the image ids
+    generated_text = blip_processor.batch_decode(
+        generated_ids, skip_special_tokens=True
+    )
+    generated_text = generated_text[0].strip()
 
-# Load Rorschach image
-url = "https://upload.wikimedia.org/wikipedia/commons/7/70/Rorschach_blot_01.jpg"
-image = Image.open(urlopen(url)).convert("RGB")
+    # Generate caption
+    inputs = blip_processor(image, return_tensors="pt").to(device, torch.float16)
+    generated_ids = model.generate(**inputs, max_new_tokens=20)
+    generated_text = blip_processor.batch_decode(
+        generated_ids, skip_special_tokens=True
+    )
+    generated_text = generated_text[0].strip()
 
-# Generate caption
-inputs = blip_processor(image, return_tensors="pt").to(device, torch.float16)
-generated_ids = model.generate(**inputs, max_new_tokens=20)
-generated_text = blip_processor.batch_decode(
-    generated_ids, skip_special_tokens=True
-)
-generated_text = generated_text[0].strip()
-generated_text
+    return generated_text
+
+generated_text = blip_model(image)
+import pdb
+pdb.set_trace()
