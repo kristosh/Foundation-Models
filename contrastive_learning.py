d5ae2afdbef48764c6ea454187a0a8537b029e7e
kristosh <chrathans@gmail.com>
2025-02-19 17:21:33 +0100

    updates

--- contrastive_learning.py
+++ contrastive_learning.py
@@ -60,5 +60,75 @@ trainer = SentenceTransformerTrainer(
 )
 trainer.train()
 
+from mteb import MTEB
+# Choose evaluation task
+evaluation = MTEB(tasks=["Banking77Classification"])
+# Calculate results
+results = evaluation.run(model)
+
+from mteb import MTEB
+# Choose evaluation task
+evaluation = MTEB(tasks=["Banking77Classification"])
+# Calculate results
+results = evaluation.run(model)
+
 import pdb
 pdb.set_trace()
+
+from datasets import Dataset, load_dataset
+# Load MNLI dataset from GLUE
+# 0 = entailment, 1 = neutral, 2 = contradiction
+train_dataset = load_dataset(
+    "glue", "mnli", split="train").select(range(50_000))
+train_dataset = train_dataset.remove_columns("idx")
+# (neutral/contradiction)=0 and (entailment)=1
+mapping = {2: 0, 1: 0, 0:1}
+train_dataset = Dataset.from_dict({
+    "sentence1": train_dataset["premise"],
+    "sentence2": train_dataset["hypothesis"],
+    "label": [float(mapping[label]) for label in
+    train_dataset["label"]]
+})
+
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+# Create an embedding similarity evaluator for stsb
+val_sts = load_dataset("glue", "stsb", split="validation")
+evaluator = EmbeddingSimilarityEvaluator(
+    sentences1=val_sts["sentence1"],
+    sentences2=val_sts["sentence2"],
+    scores=[score/5 for score in val_sts["label"]],
+    main_similarity="cosine"
+)
+
+
+from sentence_transformers import losses, SentenceTransformer
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
+# Define model
+embedding_model = SentenceTransformer("bert-base-uncased")
+# Loss function
+train_loss = losses.CosineSimilarityLoss(model=embedding_model)
+# Define the training arguments
+args = SentenceTransformerTrainingArguments(
+    output_dir="cosineloss_embedding_model",
+    num_train_epochs=1,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    warmup_steps=100,
+    fp16=True,
+    eval_steps=100,
+    logging_steps=100,
+)
+
+# Train model
+trainer = SentenceTransformerTrainer(
+    model=embedding_model,
+    args=args,
+    train_dataset=train_dataset,
+    loss=train_loss,
+    evaluator=evaluator
+)
+trainer.train()
+
+# Evaluate our trained model
+evaluator(embedding_model)
